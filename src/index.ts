8a1c28232d66d9fce97b52e3e244c1547825b342
Jesse Hoogervorst <j.hoogervorst@condifood.com>
2019-02-03 21:01:12 +0100

    Added development tools to run front and backend

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,17 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 
-app.get( "/", ( req: express.Request, res: express.Response ) => {
+app.set("port", process.env.SERVER_PORT || 3000);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/dist/pompmast-server-frontend"));
+}
+
+app.get("/", (req: express.Request, res: express.Response) => {
     res.send("Hello world!");
-} );
+});
 
 // start the express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(app.get("port"), () => {
+    console.log(`server started at http://localhost:${app.get("port")}`);
+});
